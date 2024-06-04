54a3bfa7a1a6c4cdd9c54246c955812887358bd7
UreshiiPanda <jojohoughton22@gmail.com>
2024-06-04 16:58:32 -0500

    tips page started

--- zeal/src/app/shared/components/home/home.component.ts
+++ zeal/src/app/shared/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faShip } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'home',
   standalone: true,
   imports: [
+    FontAwesomeModule,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
@@ -13,5 +17,5 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  faShip = faShip;
 }
--- zeal/src/app/shared/components/tips/tips.component.ts
+++ zeal/src/app/shared/components/tips/tips.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
   selector: 'tips',
   standalone: true,
   imports: [
+    FontAwesomeModule,
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
@@ -13,6 +16,7 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './tips.component.html',
   styleUrl: './tips.component.css'
 })
-export class TipsComponent {
 
+export class TipsComponent {
+  faDollarSign = faDollarSign;
 }
