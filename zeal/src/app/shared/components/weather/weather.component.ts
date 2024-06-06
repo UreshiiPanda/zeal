ea3b190b436783894fb2e791d206f8ddafcb4ab8
UreshiiPanda <jojohoughton22@gmail.com>
2024-06-06 16:50:24 -0500

    weather page finished except for API call

--- zeal/src/app/shared/components/weather/weather.component.ts
+++ zeal/src/app/shared/components/weather/weather.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe, NgIf } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+
 
 @Component({
   selector: 'weather',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    DecimalPipe,
+    FormsModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+
+  ],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
+
 export class WeatherComponent {
 
+  showWeather: boolean = false;
+  zipCode: string = '';
+  weatherData: any = {
+    city: 'Dallas, TX 75201',
+    temp: '25°C',
+    humidity: '90',
+    conditions: 'Partly cloudy'
+  };
+
+  getWeather() {
+    // Perform weather API call based on the zipCode
+    // Update the weatherData based on the API response
+    this.showWeather = true;
+  }
+
+  enterNewZipCode() {
+    this.showWeather = false;
+    this.zipCode = '';
+  }
 }
