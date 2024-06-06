3035bedbe78caced293a013d53e329493479b294
UreshiiPanda <jojohoughton22@gmail.com>
2024-06-06 15:34:01 -0500

    rps page started

--- zeal/src/app/shared/components/rps/rps.component.ts
+++ zeal/src/app/shared/components/rps/rps.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { faGem, faPaperPlane, faScissors } from '@fortawesome/free-solid-svg-icons';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
+
+
 
 @Component({
   selector: 'rps',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    DecimalPipe,
+    FormsModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+
+  ],
   templateUrl: './rps.component.html',
   styleUrl: './rps.component.css'
 })
 export class RpsComponent {
+  faGem = faGem;
+  faPaperPlane = faPaperPlane;
+  faScissors = faScissors;
 
 }
