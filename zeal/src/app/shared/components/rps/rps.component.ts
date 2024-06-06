4039e0bf16d81c734f95ab44cad770fee8bdb863
UreshiiPanda <jojohoughton22@gmail.com>
2024-06-06 16:14:30 -0500

    rps page finished

--- zeal/src/app/shared/components/rps/rps.component.ts
+++ zeal/src/app/shared/components/rps/rps.component.ts
@@ -3,14 +3,14 @@ import { faGem, faPaperPlane, faScissors } from '@fortawesome/free-solid-svg-ico
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
-import { DecimalPipe } from '@angular/common';
-
+import { DecimalPipe, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'rps',
   standalone: true,
   imports: [
+    NgIf,
     FontAwesomeModule,
     DecimalPipe,
     FormsModule,
@@ -27,4 +27,39 @@ export class RpsComponent {
   faPaperPlane = faPaperPlane;
   faScissors = faScissors;
 
+  userChoice: string = '';
+  computerChoice: string = '';
+  result: string = '';
+
+  makeChoice(choice: string) {
+    this.userChoice = choice;
+    this.computerChoice = this.getComputerChoice();
+    this.result = this.determineResult(choice, this.computerChoice);
+  }
+
+  getComputerChoice(): string {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+
+  determineResult(userChoice: string, computerChoice: string): string {
+    if (userChoice === computerChoice) {
+      return 'tie';
+    } else if (
+      (userChoice === 'Rock' && computerChoice === 'Scissors') ||
+      (userChoice === 'Paper' && computerChoice === 'Rock') ||
+      (userChoice === 'Scissors' && computerChoice === 'Paper')
+    ) {
+      return 'win';
+    } else {
+      return 'lose';
+    }
+  }
+
+  resetGame() {
+    this.userChoice = '';
+    this.computerChoice = '';
+    this.result = '';
+  }
 }
