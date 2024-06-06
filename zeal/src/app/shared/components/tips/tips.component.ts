60eb93364871fda40102116ff93ee7636447d8e3
UreshiiPanda <jojohoughton22@gmail.com>
2024-06-06 11:25:29 -0500

    tips page finished except for minor css tweaks

--- zeal/src/app/shared/components/tips/tips.component.ts
+++ zeal/src/app/shared/components/tips/tips.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons';
 import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 
 
 @Component({
@@ -10,6 +11,7 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [
     FontAwesomeModule,
+    DecimalPipe,
     FormsModule,
     RouterOutlet,
     RouterLink,
@@ -22,9 +24,24 @@ import { FormsModule } from '@angular/forms';
 export class TipsComponent {
   faDollarSign = faDollarSign;
   tipValue: number = 0;
+  tipAmount: number = 0.00;
+  totalBillAmount: number = 0.00;
   totalBill: number = 0;
+
   onTipChange(event: any) {
     this.tipValue = event.target.value;
+    this.calculateTipAmount();
+    this.calculateTotalBillAmount();
   }
+
+  calculateTipAmount() {
+    this.tipAmount = (this.totalBill * this.tipValue) / 100;
+  }
+
+  calculateTotalBillAmount() {
+    this.totalBillAmount = this.totalBill + this.tipAmount;
+  }
+
 }
+
 
